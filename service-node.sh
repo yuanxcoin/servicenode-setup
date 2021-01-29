531467519e5cfa4e3562c8dc4023d94448673ce4
chongada <chongada@gyuan.online>
2021-01-29 12:12:35 +0000

    improved installation

--- service-node.sh
+++ service-node.sh
@@ -1,6 +1,12 @@
-echo -n "Installation of Gyuanx service node and Gyuanxd"
+#!/bin/bash
 
-echo -n "Installation of the  Gyuanx Service Node begins"
+    echo
+    echo -e "$GREEN************************************************************************$COL_RESET"
+    echo -e "$GREEN Installation of Gyuanx service node and Gyuanxd $COL_RESET"
+    echo -e "$GREEN Everything will be compile for you $COL_RESET"
+    echo -e "$GREEN************************************************************************$COL_RESET"
+    echo
+    sleep 8
 
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y  
 
@@ -18,12 +24,15 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /u
 
 sudo apt install -y  curl git wget libssl-dev libsodium-dev wget pkg-config autoconf libtool  gperf libsystemd-dev libgtest-dev libunbound-dev nettle-dev libevent-dev libminiupnpc-dev libunwind8-dev libreadline-dev libhidapi-dev libusb-1.0-0-dev libprotobuf-dev protobuf-compiler python3 libsqlite3-dev qttools5-dev libcurl4-openssl-dev
 
-export  CC=gcc-8 
-export CXX=g++-8
+cd 
 
 wget https://cmake.org/files/v3.19/cmake-3.19.2-Linux-x86_64.tar.gz
 tar -xvf cmake-3.19.2-Linux-x86_64.tar.gz
-export PATH="`pwd`/cmake-3.19.2-Linux-x86_64/bin:$PATH"
+export PATH=~/cmake-3.19.2-Linux-x86_64/bin:$PATH
+cd
+echo 'export PATH=~/cmake-3.19.2-Linux-x86_64/bin:$PATH' >> ~/.bashrc
+
+source .bashrc
 
 export  BOOST_VERSION=1_70_0
 export BOOST_VERSION_DOT=1.70.0
@@ -33,12 +42,17 @@ curl -s -L -o  boost_${BOOST_VERSION}.tar.bz2 https://dl.bintray.com/boostorg/re
 echo "${BOOST_HASH}  boost_${BOOST_VERSION}.tar.bz2" | sha256sum -c
 tar -xvf boost_${BOOST_VERSION}.tar.bz2
 cd boost_${BOOST_VERSION} 
+export  CC=gcc-8 
+export CXX=g++-8
 ./bootstrap.sh 
 sudo  ./b2 install
-export  BOOST_ROOT /usr/local/include/boost
 
-cd ..
+cd
+export  BOOST_ROOT=/usr/local/include/boost
+echo 'export  BOOST_ROOT=/usr/local/include/boost' >> ~/.bashrc
+source .bashrc
 
+cd
 export  OPENSSL_VERSION=1.1.1c
 export OPENSSL_HASH=f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90
      curl -s -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz 
@@ -48,10 +62,12 @@ export OPENSSL_HASH=f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f
      ./Configure linux-x86_64 no-shared --static -fPIC
      make build_generated
      make libcrypto.a
-     make install
- export OPENSSL_ROOT_DIR=/usr/local/openssl-${OPENSSL_VERSION}
-
-cd ..
+     sudo make install
+cd
+export OPENSSL_ROOT_DIR=/usr/local/openssl-${OPENSSL_VERSION}
+echo 'export OPENSSL_ROOT_DIR=/usr/local/openssl-${OPENSSL_VERSION}' >> ~/.bashrc
+source .bashrc
+cd
 
 # Sodium
  export  SODIUM_VERSION=1.0.18
@@ -63,17 +79,17 @@ cd ..
      CFLAGS="-fPIC" CXXFLAGS="-fPIC" ./configure
      make 
      make check 
-     make install
-cd ..
+     sudo make install
+cd
 
 rm -rf gyuanx-storage-server
  git clone https://github.com/yuanxcoin/gyuanx-storage-server.git --depth=1 -b dev
 
   cd gyuanx-storage-server && git submodule update --init --recursive
 
- cd gyuanx-storage-server 
-     mkdir -p build 
-     cd build 
+ cd gyuanx-storage-server
+     mkdir -p build
+     cd build
      cmake .. -DBOOST_ROOT=$BOOST_ROOT -Dsodium_USE_STATIC_LIBS=ON 
      cmake --build . -- -j1
  sudo make install
@@ -83,21 +99,32 @@ cd ../..
 echo -n "Service Node successfully Installed"
 echo -n "Installation of the  gyuanx core begins"
 
+    clear
+    echo
+    echo -e "$GREEN************************************************************************$COL_RESET"
+    echo -e "$GREEN Service Node successfully Installed in your PC$COL_RESET"
+    echo -e "$GREEN Installation of the  gyuanx core begins $COL_RESET"
+    echo -e "$GREEN************************************************************************$COL_RESET"
+    echo
+    sleep 6
+
 git clone --recursive https://github.com/yuanxcoin/gyuanx-core 
 
 cd gyuanx-core 
 
 mkdir build && cd build
 
-cmake .. -DBUILD_STATIC_DEPS=ON -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8 -DARCH=x86-64
+cmake .. -DBUILD_STATIC_DEPS=ON -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8
 
 make 
 
 sudo make install 
 
-
-echo -n "Service node and gyuanx wallet successfully installed"
-
-echo -n "Run the service node with (gyuanx-storage your-ip-address  8080  --lmq-port 11111)"
-
-echo -n "Run the gyuanx node with (gyuanxd --service-node --storage-server-port 11115 --service-node-public-ip your-ip-address) "
+    clear
+    echo
+    echo -e "$GREEN*********************************************************************************************************************$COL_RESET"
+    echo -e "$GREEN Service node and Gyuanx wallet successfully installed $COL_RESET"
+    echo -e "$RED Run the service node with: gyuanx-storage your-ip-address  8080  --lmq-port 11111 $COL_RESET"
+    echo -e "$RED Run the gyuanx node with: gyuanxd --service-node --storage-server-port 11115 --service-node-public-ip your-ip-address $COL_RESET"
+    echo -e "$GREEN*********************************************************************************************************************$COL_RESET"
+    echo
